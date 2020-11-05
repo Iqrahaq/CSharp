71129fd6772aec7fe23e11321c7d74536b8be296
iqrahaq <iqra.haq@northumbria.ac.uk>
2020-11-05 19:11:33 +0000

    IKeyGUI  file sorted

--- week5/ApplicationNonMVP/WindowsFormsApplication1/WindowsFormsApplication1/IKeyGUI.cs
+++ week5/ApplicationNonMVP/WindowsFormsApplication1/WindowsFormsApplication1/IKeyGUI.cs
@@ -9,6 +9,18 @@ namespace WindowsFormsApplication1
         Boolean numbers { get; set; }
         Boolean otherChars { get; set; }
         Boolean hex { get; set; }
-        
+
+        void setSeconds(string seconds);
+        void setHours(string hours);
+        void setDays(string days);
+        void setMonths(string months);
+        void setYears(string years);
+        int getKeyLength();
+        void setKeyLength(int keyLength);
+        UInt64 getAttemptsPerSecond();
+        void setAttempsPerSecond(UInt64 attempts);
+        void setNewKey(string key);
+        string getNewKey();
+
     }
 }
--- week5/ApplicationNonMVP/WindowsFormsApplication1/WindowsFormsApplication1/KeyGenPresenter.cs
+++ week5/ApplicationNonMVP/WindowsFormsApplication1/WindowsFormsApplication1/KeyGenPresenter.cs
@@ -5,9 +5,67 @@ using System.Text;
 
 namespace WindowsFormsApplication1
 {
-    class KeyGenPresenter
+    class KeyGenPresenter : IKeyGUI
     {
-         
+        public bool uppercase { get; set; }
+        public bool lowercase { get; set; }
+        public bool numbers { get; set; }
+        public bool otherChars { get; set; }
+        public bool hex { get; set; }
 
+        public ulong getAttemptsPerSecond()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getKeyLength()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getNewKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setAttempsPerSecond(ulong attempts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setDays(string days)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setHours(string hours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setKeyLength(int keyLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setMonths(string months)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setNewKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setSeconds(string seconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setYears(string years)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
