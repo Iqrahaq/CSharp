c9e2b2f95c62f23304fe45d24963ea2fa95ad477
Iqra <iqra.haq@northumbria.ac.uk>
2020-12-03 18:21:35 +0000

    week 10 Lab 1 complete

---
+++ week10/CodeFirstEF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDBEntities())
+            {
+                var studentList = context.Students.ToList<Student>();
+                Address a = new Address();
+                a.AddressId = 1;
+                a.NumberorName = "20";
+                a.PostCode = "LL00 GP4";
+                a.Street = "BackOfBehold";
+                a.Town = "Trumpton";
+
+                context.Addresses.Add(a);
+                context.SaveChanges();
+            }
+        }
+    }
+}
